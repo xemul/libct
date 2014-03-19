655102432f777954427c7c9e4afc189d6fe86803
Pavel Emelyanov <xemul@parallels.com>
2014-03-19 17:03:19 +0400

    test: Check that proc, mount namespaces and proc work well
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/ct_proc.c
@@ -0,0 +1,63 @@
+#include <libct.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+#include <sched.h>
+
+#ifndef CLONE_NEWNS
+#define CLONE_NEWNS     0x00020000
+#endif  
+                
+#ifndef CLONE_NEWPID
+#define CLONE_NEWPID    0x20000000
+#endif
+
+static int set_ct_root_pids(void *a)
+{
+	int *pids = a;
+	char buf[32];
+
+	memset(buf, 0, sizeof(buf));
+	pids[0] = getpid();
+	readlink("/proc/self", buf, sizeof(buf));
+	pids[1] = atoi(buf);
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int *ct_root_pids;
+	libct_session_t s;
+	ct_handler_t ct;
+
+	ct_root_pids = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+			MAP_SHARED | MAP_ANON, 0, 0);
+	ct_root_pids[0] = 0;
+	ct_root_pids[1] = 0;
+
+	libct_init();
+	s = libct_session_open_local();
+	ct = libct_container_create(s);
+	if (libct_container_set_nsmask(ct, CLONE_NEWPID | CLONE_NEWNS)) {
+		printf("No pid & mount NS here\nERROR\n");
+		return 0;
+	}
+
+	libct_container_spawn(ct, set_ct_root_pids, ct_root_pids);
+	libct_container_join(ct);
+	libct_container_destroy(ct);
+	libct_session_close(s);
+	libct_exit();
+
+	/* Should be init */
+	if ((ct_root_pids[0] != 1) || (ct_root_pids[1] != 1)) {
+		printf("Pid mismatch %d/%d\nFAIL\n", ct_root_pids[0], ct_root_pids[1]);
+		return 1;
+	}
+
+	printf("Pids are valid (%d/%d)\nPASS\n",
+			ct_root_pids[0], ct_root_pids[1]);
+	return 0;
+}
