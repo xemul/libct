2b0328bf213167d8234232174c397d59d6a099ec
Pavel Emelyanov <xemul@parallels.com>
2014-03-21 05:50:31 +0400

    test: Don't use _kill callback
    It's nasty, w/o pidns it doesn't work (going to fix it)
    so stop using it for test containers.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/ct_enter.c
+++ test/ct_enter.c
@@ -4,46 +4,56 @@
 #include <sys/wait.h>
 #include "test.h"
 
+struct ct_arg {
+	int wait_fd;
+	int *mark;
+};
+
 static int set_ct_alive(void *a)
 {
-	*(int *)a = 1;
-	while (1)
-		sleep(1);
+	struct ct_arg *cta = a;
+	char c;
+
+	cta->mark[0] = 1;
+	read(cta->wait_fd, &c, 1);
 	return 0;
 }
 
 static int set_ct_enter(void *a)
 {
-	*((int *)a + 1) = 1;
+	struct ct_arg *cta = a;
+	cta->mark[1] = 1;
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	int *ct_alive;
-	int pid;
+	struct ct_arg cta;
+	int pid, p[2];
 	libct_session_t s;
 	ct_handler_t ct;
 
-	ct_alive = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+	pipe(p);
+	cta.mark = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
 			MAP_SHARED | MAP_ANON, 0, 0);
-	ct_alive[0] = 0;
-	ct_alive[1] = 0;
+	cta.mark[0] = 0;
+	cta.mark[1] = 0;
+	cta.wait_fd = p[0];
 
 	s = libct_session_open_local();
 	ct = libct_container_create(s);
-	libct_container_spawn_cb(ct, set_ct_alive, ct_alive);
-	pid = libct_container_enter(ct, set_ct_enter, ct_alive);
+	libct_container_spawn_cb(ct, set_ct_alive, &cta);
+	pid = libct_container_enter(ct, set_ct_enter, &cta);
 	waitpid(pid, NULL, 0);
-	libct_container_kill(ct);
+	write(p[1], "a", 1);
 	libct_container_wait(ct);
 	libct_container_destroy(ct);
 	libct_session_close(s);
 
-	if (!ct_alive[0])
+	if (!cta.mark[0])
 		return fail("CT is not alive");
 
-	if (!ct_alive[1])
+	if (!cta.mark[1])
 		return fail("CT is not enterable");
 
 	return pass("CT is created and entered");
--- test/ct_root_enter.c
+++ test/ct_root_enter.c
@@ -10,8 +10,14 @@
 #define FS_ROOT	"libct_test_root"
 #define FS_DATA	"libct_test_string"
 #define FS_FILE "file"
+#define ENTER_DOFF	1024
 
-static int check_fs_enter_data(void *a)
+struct ct_arg {
+	int wait_fd;
+	char *fs_data;
+};
+
+static int read_fs_data(char *a)
 {
 	int fd;
 
@@ -21,26 +27,37 @@ static int check_fs_enter_data(void *a)
 
 	read(fd, a, sizeof(FS_DATA));
 	close(fd);
+
 	return 0;
 }
 
-static int check_fs_data(void *a)
+static int ct_main_fn(void *a)
 {
-	if (check_fs_enter_data(a))
+	struct ct_arg *cta = a;
+	char c;
+
+	if (read_fs_data(cta->fs_data))
 		return 1;
 
-	while (1)
-		sleep(1);
-
+	read(cta->wait_fd, &c, 1);
 	return 0;
+}
+
+static int ct_enter_fn(void *a)
+{
+	struct ct_arg *cta = a;
+
+	return read_fs_data(cta->fs_data + ENTER_DOFF);
 }
 
 int main(int argc, char **argv)
 {
-	int fd, pid;
-	char *fs_data;
+	int fd, pid, p[2];
+	struct ct_arg cta;
 	libct_session_t s;
 	ct_handler_t ct;
+
+	pipe(p);
 
 	mkdir(FS_ROOT);
 	fd = open(FS_ROOT "/" FS_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -50,18 +67,19 @@ int main(int argc, char **argv)
 	write(fd, FS_DATA, sizeof(FS_DATA));
 	close(fd);
 
-	fs_data = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+	cta.fs_data = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
 			MAP_SHARED | MAP_ANON, 0, 0);
-	fs_data[0] = '\0';
-	fs_data[1024] = '\0';
+	cta.fs_data[0] = '\0';
+	cta.fs_data[ENTER_DOFF] = '\0';
+	cta.wait_fd = p[0];
 
 	s = libct_session_open_local();
 	ct = libct_container_create(s);
 	libct_fs_set_root(ct, FS_ROOT);
-	libct_container_spawn_cb(ct, check_fs_data, fs_data);
-	pid = libct_container_enter(ct, check_fs_enter_data, fs_data + 1024);
+	libct_container_spawn_cb(ct, ct_main_fn, &cta);
+	pid = libct_container_enter(ct, ct_enter_fn, &cta);
 	waitpid(pid, NULL, 0);
-	libct_container_kill(ct);
+	write(p[1], "a", 1);
 	libct_container_wait(ct);
 	libct_container_destroy(ct);
 	libct_session_close(s);
@@ -69,10 +87,10 @@ int main(int argc, char **argv)
 	unlink(FS_ROOT "/" FS_FILE);
 	rmdir(FS_ROOT);
 
-	if (strcmp(fs_data, FS_DATA))
+	if (strcmp(cta.fs_data, FS_DATA))
 		return fail("FS not accessed");
 
-	if (strcmp(fs_data + 1024, FS_DATA))
+	if (strcmp(cta.fs_data + ENTER_DOFF, FS_DATA))
 		return fail("FS not entered");
 
 	return pass("FS is created and entered");
