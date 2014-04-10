5713406d3fd105a2604480cc8439afbfc8240b7d
Pavel Emelyanov <xemul@parallels.com>
2014-04-10 11:17:45 +0400

    test: Add test for killing container w/o pid namespace
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/ct_kill_nons.c
@@ -0,0 +1,84 @@
+/*
+ * Test that service cgroup works
+ */
+#include <libct.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include "test.h"
+
+struct ct_arg {
+	int start_fd;
+	int *mark;
+};
+
+static int loop_in_ct(void *a)
+{
+	struct ct_arg *cta = a;
+	char c = 'a';
+	int cpid;
+
+	cpid = fork();
+	if (cpid == 0)
+		goto loop;
+
+	cta->mark[0] = getpid();
+	cta->mark[1] = cpid;
+
+	write(cta->start_fd, &c, 1);
+
+loop:
+	/*
+	 * Don't close the pipe. If killing cgroup
+	 * failed, test would hang forever FIXME
+	 */
+
+	while (1)
+		sleep(10);
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	struct ct_arg cta;
+	int p[2];
+	libct_session_t s;
+	ct_handler_t ct;
+	char c;
+
+	pipe(p);
+	cta.mark = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+			MAP_SHARED | MAP_ANON, 0, 0);
+	cta.mark[0] = 0;
+	cta.mark[1] = 0;
+	cta.start_fd = p[1];
+
+	s = libct_session_open_local();
+	ct = libct_container_create(s, "test-k");
+	if (libct_container_set_option(ct, LIBCT_OPT_KILLABLE))
+		return err("can't set killable");
+
+	if (libct_container_spawn_cb(ct, loop_in_ct, &cta))
+		return err("can't start CT");
+
+	close(p[1]);
+	read(p[0], &c, 1);
+
+	libct_container_kill(ct);
+	libct_container_wait(ct);
+	libct_container_destroy(ct);
+	libct_session_close(s);
+
+	if (read(p[0], &c, 1) != 0) /* FIXME -- this may block on error */
+		return fail("Pipes are alive?");
+
+	if (!cta.mark[0])
+		return fail("CT is not alive");
+
+	if (!cta.mark[1])
+		return fail("CT hasn't forked");
+
+	return pass("killed OK");
+}
