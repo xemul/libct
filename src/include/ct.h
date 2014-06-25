5d169c0726e3281b5c8d646a594cb666d08064e2
Andrey Vagin <avagin@openvz.org>
2014-06-25 18:03:22 +0400

    ct: fix value of CT_NOSETSID
    CT_NOSETSID has to contain one bit
    
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- src/include/ct.h
+++ src/include/ct.h
@@ -44,7 +44,7 @@ ct_handler_t ct_create(char *name);
 
 #define CT_AUTO_PROC		0x1
 #define CT_KILLABLE		0x2
-#define CT_NOSETSID		0x3
+#define CT_NOSETSID		0x4
 
 /*
  * The main structure describing a container
